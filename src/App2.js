3b16c341161d46621b7574cfd4450215bb0bfa38
Dario Del Piano <dario@metacell.us>
2022-05-10 13:32:12 +0100

    second commit

---
+++ src/App2.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App2() {
+  return (
+    <div className="App">
+      <div className="topBar"></div>
+      <div className="main"></div>
+    </div>
+  );
+}
+
+export default App2;
